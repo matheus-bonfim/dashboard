c4ef879ee6d89fd8819f94d224308eede33c8a95
matheus-bonfim <mcorreiacruzbonfim@gmail.com>
2024-10-17 08:08:35 -0300

    grafico sem bugs

--- src/App.js
+++ src/App.js
@@ -7,17 +7,39 @@ import {LineChart, GraficoCotacao} from './grafico.js';
 
 let moeda = 'EUR-BRL'; 
 
+
 function App() {
   // Definindo estados para o conteúdo e a cotação
   const [content, setContent] = useState('Bem-vindo! Selecione um item no menu.');
   const [dados, setDados] = useState(null); // Estado para armazenar o preço da cotação
   const [loading, setLoading] = useState(false); // Estado para mostrar se os dados estão carregando
   const [refresh, setRefresh] = useState(false);
+  const [refresh_cot, setRefresh_cot] = useState(false);
+  const [dados_dias, setDados_dias] = useState(null);
+  const [input_dias, setInput_dias] = useState(30);
+  const [temp_input_dias, setTemp_input_dias] = useState(30);
+  
+  
+  
   // Função para alterar o conteúdo
   const handleContentChange = (newContent) => {
     setContent(newContent);
   };
-
+  const handle_input_enter = (e) => {
+    if (e.key === 'Enter'){
+      const days_ = parseInt(temp_input_dias);
+      if(!isNaN(days_) && days_ > 0 && days_ < 366){
+        setInput_dias(temp_input_dias);
+        //setRefresh_cot(prev => !prev);
+        }
+      else{
+        alert('INSIRA UM NÚMERO VÁLIDO!');
+      }
+    }
+  }
+  const handle_input_barra = (e) => {
+    setTemp_input_dias(e.target.value); 
+  }
   // useEffect para buscar os dados da cotação quando o conteúdo for 'Cotação'
   useEffect(() => {
     const fetchData = async () => {
@@ -38,17 +60,20 @@ function App() {
     const fetchData_ = async () => {
       if (content === 'Gráfico Cotação') {
         setLoading(true); // Inicia o estado de carregamento
-        const data = await get_coin_info_list('EUR-BRL',50);
-        if (data) {
-          setDados(data); // Define o preço no estado
+        console.log("input_dias:" + input_dias);
+        const data_dias = await get_coin_info_list('EUR-BRL',input_dias);
+        if (data_dias) {
+          setDados_dias(data_dias);
+          console.log("66:    " + data_dias);
         }
         setLoading(false); // Finaliza o estado de carregamento
       }
     };
 
     fetchData_(); // Chama a função assíncrona
-  }, [content]); // O useEffect depende da mudança de 'content'
+  }, [content, input_dias]); // O useEffect depende da mudança de 'content'
 
+  
   let displayContent;
   if (content === 'Bem-vindo! Selecione um item no menu.') {
     displayContent = 
@@ -65,16 +90,36 @@ function App() {
     </div>);
    
   } else if (content === 'Gráfico Cotação'){
+    
     if (loading){
       displayContent = <h1>Carregando cotação...</h1>;
     }
-    else if(dados){
-      console.log('linha 72:' + dados);
+    
+    else if(dados_dias && dados_dias[0].high){
+      console.log('linha 72:' + dados_dias);
       displayContent = (
-        <div className="grafico">
-          <h1>Gráfico Cotação</h1>
-          <GraficoCotacao list={dados} days={50} /> 
-        </div>);
+        <div> 
+          <div className="grafico">
+            <h1>Gráfico Cotação</h1>
+            <GraficoCotacao list={dados_dias}/> 
+          </div>
+          <div className="barra_busca">
+            <input
+                type="text"
+                value={temp_input_dias}
+                onChange={handle_input_barra} // Atualiza o estado com o valor digitado
+                onKeyDown={handle_input_enter} // Captura a tecla pressionada
+                placeholder="Digite algo e pressione Enter"
+                min="1"
+                max="366"
+            />
+          </div>
+        </div>
+
+         
+      
+      
+      );
     }
     else {
       displayContent = <h1>Erro ao buscar a cotação</h1>;
--- src/grafico.js
+++ src/grafico.js
@@ -40,49 +40,56 @@ const LineChart = () => {
 
 const GraficoCotacao = (response) => {
     // Dados do gráfico
-    let high_list = [];
-    let days_list = [];
-    let days = parseInt(response.days);
-    //let resp_list = JSON.stringify(response.list);
-    console.log('graf li 46:' + response.list);
-    //console.log('tamanho' + response.list.lenght);
-    for(let i=0; i<days; i++){
-        high_list[days-1-i] = parseFloat(response.list[i].high);
-        console.log("dentro do loop :"+ response.list[i].high);
-    }
-    for(let i=0; i<days; i++){
-      days_list[i] = i; 
-    }
-    console.log("high_list:"+high_list);
+    if (response.list){
+      let days = parseInt(response.list.length);
+      let high_list = [];
+      let days_list = [];
+      //let days = parseInt(response.days);
+      console.log("tamnho da lista é: " + response.list.length);
+      //let resp_list = JSON.stringify(response.list);
+      console.log('graf li 46:' + response.list);
+      //console.log('tamanho' + response.list.lenght);
+      for(let i=0; i<days; i++){
+          high_list[days-1-i] = parseFloat(response.list[i].high);
+          //console.log("dentro do loop :"+ response.list[i].high);
+      }
+      for(let i=0; i<days; i++){
+        days_list[i] = i; 
+      }
+      console.log("high_list:"+high_list);
 
-    const data = {
-      labels: days_list, // Eixo X
-      datasets: [
-        {
-          label: 'Vendas em 2024',
-          data: high_list, // Eixo Y
-          fill: false,
-          borderColor: 'rgba(75,192,192,1)',
-          tension: 0.1,
+      const data = {
+        labels: days_list, // Eixo X
+        datasets: [
+          {
+            label: 'Alta nos últimos dias',
+            data: high_list, // Eixo Y
+            fill: false,
+            borderColor: 'rgba(75,192,192,1)',
+            tension: 0.1,
+          },
+        ],
+      };
+    
+      // Opções do gráfico
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'EUR/BRL',
+          },
         },
-      ],
-    };
-  
-    // Opções do gráfico
-    const options = {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Gráfico de Vendas',
-        },
-      },
-    };
-  
-    return <Line data={data} options={options} />;
+      };
+    
+      return <Line data={data} options={options} />;
+    }
+    else{
+      console.log("fail");
+    }
   };
 
 export {LineChart};
